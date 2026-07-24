04272af96a2aae5cc71d1c43dc5e6ba5f7bd5352
Imre Kis <imre.kis@arm.com>
2026-07-24 15:19:29 +0200

    Rename variable to static_item
    Signed-off-by: Imre Kis <imre.kis@arm.com>
    Change-Id: I862dd933a36083ebcbd542773283cb002e6ca714

--- percore-derive/src/lib.rs
+++ percore-derive/src/lib.rs
@@ -13,13 +13,13 @@ use syn::{ItemStatic, ItemStruct, parse_macro_input};
 /// and can be used to access it from assembly.
 #[proc_macro_attribute]
 pub fn percore(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    let variable = parse_macro_input!(item as ItemStatic);
+    let static_item = parse_macro_input!(item as ItemStatic);
 
-    let attrs = &variable.attrs;
-    let vis = &variable.vis;
-    let name = &variable.ident;
-    let ty = &variable.ty;
-    let expr = &variable.expr;
+    let attrs = &static_item.attrs;
+    let vis = &static_item.vis;
+    let name = &static_item.ident;
+    let ty = &static_item.ty;
+    let expr = &static_item.expr;
 
     quote! {
         #[cfg_attr(target_os = "none", unsafe(link_section = ".percore"))]
